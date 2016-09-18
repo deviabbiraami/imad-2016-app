47de468dd482cbb3f58f709b9dba438d79db4e96
deviabbiraami <deviabbiraami@gmail.com>
2016-09-18 22:47:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article1', function (req) {
+   res.send("Article1 request will be served here"); 
+});
+
+app.get('/Article2', function (req) {
+   res.send("Article2 request will be served here"); 
+});
+
+app.get('/Article3', function (req) {
+   res.send("Article3 request will be served here"); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
